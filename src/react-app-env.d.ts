5e1f8d643ae34a65fb00192345fb9d593613fcd9
rui-watanabe <wataru610806@gmail.com>
2021-03-28 19:09:37 +0900

    import component #2

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+/// <reference types="node" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    // readonly REACT_APP_NODE_ENV: 'development' | 'production' | 'test';
+    readonly REACT_APP_JAPAN_HOST: string;
+    readonly REACT_APP_WORLD_HOST: string;
+  }
+}
